f31478474fb0b06abb0caf3273a5ad7364db216c
Stefan Weil <sw@weilnetz.de>
2024-09-06 11:50:13 +0200

    Fix download URL for ddb_validierung_mets-mods-ap-digitalisierte-zeitungen.xsl
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- scripts/validate/install.sh
+++ scripts/validate/install.sh
@@ -2,7 +2,7 @@
 
 # Get Schematron validation files.
 curl -O https://raw.githubusercontent.com/Deutsche-Digitale-Bibliothek/ddb-metadata-schematron-validation/main/mets-mods-ap-digitalisierte-medien/ddb_validierung_mets-mods-ap-digitalisierte-medien.xsl
-curl -O https://github.com/Deutsche-Digitale-Bibliothek/ddb-metadata-schematron-validation/blob/main/mets-mods-ap-digitalisierte-zeitungen/ddb_validierung_mets-mods-ap-digitalisierte-zeitungen.xsl
+curl -O https://raw.githubusercontent.com/Deutsche-Digitale-Bibliothek/ddb-metadata-schematron-validation/main/mets-mods-ap-digitalisierte-zeitungen/ddb_validierung_mets-mods-ap-digitalisierte-zeitungen.xsl
 
 # Get XSL for text output.
 curl -O https://raw.githubusercontent.com/CSIRO-enviro-informatics/validation-svc-base/master/format/format-svrl-output-to-text.xsl
